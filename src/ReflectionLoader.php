5c4cd347845be3d3fb93e2cc12a9e3462eef7462
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-02-19 17:02:42 +0100

    wip

--- src/ReflectionLoader.php
+++ src/ReflectionLoader.php
@@ -12,7 +12,7 @@ abstract class ReflectionLoader implements LoaderInterface
 {
     public static function loadIntoProvider(): void
     {
-        with(new static)->providers()->each(fn (Closure $macro, string $name) => static::provider()::macro($name, $macro));
+        with(new static)->provides()->each(fn (Closure $macro, string $name) => static::provider()::macro($name, $macro));
     }
 
     public function provides(): Collection
